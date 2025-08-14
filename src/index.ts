1bd434dfe388a40ca71438bd2b2ae2fbd6f176d1
dhirajsharma1234 <dhirajsharma20042001@gmail.com>
2025-08-14 14:01:04 -0700

    add ssl part

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@
 import "dotenv/config";
 import app from "./app";
 import { connectDb } from "./db/conn";
+import https from "https";
+import fs from "fs";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, async () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+const sslOptions = {
+    key: fs.readFileSync(
+        "/etc/letsencrypt/live/api.gadidikhao.com/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+        "/etc/letsencrypt/live/api.gadidikhao.com/fullchain.pem"
+    ),
+};
+
+https.createServer(sslOptions, app).listen(PORT, async () => {
+    console.log(`🚀 Secure server running on port ${PORT}`);
     await connectDb();
 });
