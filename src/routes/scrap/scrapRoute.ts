60f87c31ceb21d01cf36b8c26d4ce6048296b449
dhirajsharma1234 <dhirajsharma20042001@gmail.com>
2025-09-07 10:47:40 -0700

    .

--- src/routes/scrap/scrapRoute.ts
+++ src/routes/scrap/scrapRoute.ts
@@ -1,8 +1,6 @@
 /** @format */
 
 import express from "express";
-import { CarController } from "../../controller/car";
-import { createUploadMiddleware } from "../../services/multer";
 import { scrapController } from "../../controller/scrap";
 
 const router = express.Router();
